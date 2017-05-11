ada80371ea4f4ce749f4936beff9b565b19e3e4a
Thomas Pritchard <thomas@Thomass-MacBook-Pro.local>
2017-05-11 09:47:49 -0600

    Added jquery animations to nav bar and scrolling

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	console.log("Loaded Document");
+
+	var $root = $('html, body');
+
+    $("li.nav-li").mouseover(function(){
+        $(this).stop().animate({right: '15px'}, 150);
+    });
+    $("li.nav-li").mouseleave(function(){
+        $(this).stop().animate({right: '0px'}, 100);
+    });
+
+	$('a.nav-a').click(function(event){
+		console.log("click called!");
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		console.log(sectionID);
+		scrollToID('#' + sectionID, 750);
+	});
+
+	// scroll function
+	function scrollToID(id, speed){
+		console.log("Scroll to ID called");
+		var offSet = 50;
+		var targetOffset = $(id).offset().top - offSet;
+		console.log(targetOffset);
+		$root.animate({scrollTop:targetOffset}, speed);
+	}
+});
